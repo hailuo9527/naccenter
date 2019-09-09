ff680543c0f53db3438b0771885501fa0eb85ec5
imjohn <imjohn_ls@163.com>
2019-09-09 16:58:34 +0800

    dhcp

--- src/view/chart/components/dhcpConfig.vue
+++ src/view/chart/components/dhcpConfig.vue
@@ -1,8 +1,48 @@
 <template>
   <div style="padding: 20px">
     <!--固定ip-->
+    <div class="nav-content">
+      <div class="form-group">
+    <Row :gutter="30">
+      <Col span="12">
+        <div class="form-item">
+          <label for="" class="my-label">系统状态:</label>
+          <i-switch v-model="statusList.dhcp"/>
+        </div>
+      </Col>
+      <Col span="12">
+
+      </Col>
+    </Row>
+    <Row :gutter="30">
+      <Col span="12">
+        <div class="form-item">
+          网关地址: <span>{{statusList.dhcpGateway}}</span>
+        </div>
+      </Col>
+      <Col span="12">
+        <div class="form-item">
+          DNS服务地址: <span>{{statusList.dhcpDnsServer}}</span>
+        </div>
+      </Col>
+    </Row>
+    <Row :gutter="30">
+      <Col span="12">
+        <div class="form-item">
+          起始ip: <span>{{statusList.ipStart}}</span>
+        </div>
+      </Col>
+      <Col span="12">
+        <div class="form-item">
+          结束ip: <span>{{statusList.ipEnd}}</span>
+        </div>
+      </Col>
+    </Row>
+      </div>
+    </div>
+
       <Row class="list-head" type="flex" justify="space-between" align="top">
-        <Col span="6"><h3>DHCP信息列表:</h3></Col>
+        <Col span="6"><h3>租约信息列表:</h3></Col>
       </Row>
       <Row class="table-container">
         <Table :columns="table" :data="tableList" :loading="loading" height="300" stripe
@@ -17,59 +57,69 @@
 </template>
 
 <script>
-  import { getDHCPInfo } from "../../../api/chart";
+import { getDHCPInfo, getSystemStatus } from '../../../api/chart'
 
-  export default {
-    name: 'dhcpConfig',
-    data () {
-      return {
-        loading: false,
-        table: [
-          {
-            type: 'index',
-            width: 60
-          },
-          {
-            title: 'IP地址',
-            key: 'dhcpIp'
-          },
-          {
-            title: 'MAC地址',
-            key: 'dhcpMac'
-          },
-          {
-            title: '到期时间',
-            key: 'remainingTime'
-          },
-          {
-            title: '主机名',
-            key: 'hostName'
-          }
-        ],
-        tableList: []
-      }
-    },
-    props: {
-      nbCode: {
-        type: String,
-        default: ''
-      }
-    },
-    methods: {
-      // 获取配置信息
-      async getDHCPInfo () {
-        this.loading = true
-        let res = await getDHCPInfo({ nbCode: this.nbCode })
-        this.loading = false
-        if (res.data.code === 'success') {
-          this.tableList = res.data.result || []
+export default {
+  name: 'dhcpConfig',
+  data () {
+    return {
+      loading: false,
+      table: [
+        {
+          type: 'index',
+          width: 60
+        },
+        {
+          title: 'IP地址',
+          key: 'dhcpIp'
+        },
+        {
+          title: 'MAC地址',
+          key: 'dhcpMac'
+        },
+        {
+          title: '到期时间',
+          key: 'remainingTime'
+        },
+        {
+          title: '主机名',
+          key: 'hostName'
         }
+      ],
+      tableList: [],
+	      statusList: {
+	        }
+    }
+  },
+  props: {
+    nbCode: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    // 获取配置信息
+    async getDHCPInfo () {
+      this.loading = true
+      let res = await getDHCPInfo({ nbCode: this.nbCode })
+      this.loading = false
+      if (res.data.code === 'success') {
+        this.tableList = res.data.result || []
       }
     },
-    mounted () {
-      this.getDHCPInfo()
-    }
+	    // 获取系统状态
+	    async getSystemStatus () {
+		    let res = await getSystemStatus({ nbCode: this.nbCode })
+		    if (res.data.code === 'success') {
+			    this.statusList = res.data.result || []
+		    }
+	    }
+  },
+  mounted () {
+    this.getDHCPInfo(),
+    this.getSystemStatus()
   }
+}
 </script>
 <style lang="less" scoped>
   @import "../../config/config.less";
--- src/view/chart/components/vistorInfo.vue
+++ src/view/chart/components/vistorInfo.vue
@@ -1,6 +1,32 @@
 <template>
   <div style="padding: 20px">
     <!--固定ip-->
+    <Row :gutter="30">
+      <Col span="12">
+        <div class="form-item">
+          <label for="" class="my-label">vistor:</label>
+          <i-switch disabled v-model="vistorList.dhcp"/>
+        </div>
+      </Col>
+      <Col span="12">
+        <div class="form-item">
+
+        </div>
+      </Col>
+    </Row>
+    <Row :gutter="30">
+      <Col span="12">
+        <div class="form-item">
+          dhcpGateway: <span>{{vistorList.visitDuration}}</span>
+        </div>
+      </Col>
+      <Col span="12">
+        <div class="form-item">
+          dhcpDnsServer: <span>{{vistorList.visitCount}}</span>
+        </div>
+      </Col>
+    </Row>
+
     <Row class="list-head" type="flex" justify="space-between" align="top">
       <Col span="6"><h3>DHCP信息列表:</h3></Col>
     </Row>
@@ -17,44 +43,53 @@
 </template>
 
 <script>
-  import { getVistorInfo } from "../../../api/chart";
+import { getVistorInfo, getSystemStatus } from '../../../api/chart'
 
-  export default {
-    name: 'dhcpConfig',
-    data () {
-      return {
-        loading: false,
-        table: [
-          {
-            type: 'index',
-            width: 60
-          },
-
-        ],
-        tableList: []
-      }
-    },
-    props: {
-      nbCode: {
-        type: String,
-        default: ''
-      }
-    },
-    methods: {
-      // 获取配置信息
-      async getVistorInfo () {
-        this.loading = true
-        let res = await getVistorInfo({ nbCode: this.nbCode })
-        this.loading = false
-        if (res.data.code === 'success') {
-          this.tableList = res.data.result || []
+export default {
+  name: 'dhcpConfig',
+  data () {
+    return {
+      loading: false,
+      table: [
+        {
+          type: 'index',
+          width: 60
         }
+
+      ],
+      tableList: [],
+	      vistorList: []
+    }
+  },
+  props: {
+    nbCode: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    // 获取配置信息
+    async getVistorInfo () {
+      this.loading = true
+      let res = await getVistorInfo({ nbCode: this.nbCode })
+      this.loading = false
+      if (res.data.code === 'success') {
+        this.tableList = res.data.result || []
       }
     },
-    mounted () {
-      this.getVistorInfo()
-    }
+	    // 获取系统状态
+	    async getSystemStatus () {
+		    let res = await getSystemStatus({ nbCode: this.nbCode })
+		    if (res.data.code === 'success') {
+			    this.vistorList = res.data.result || []
+		    }
+	    }
+  },
+  mounted () {
+    this.getVistorInfo(),
+    this.getSystemStatus()
   }
+}
 </script>
 <style lang="less" scoped>
   @import "../../config/config.less";
