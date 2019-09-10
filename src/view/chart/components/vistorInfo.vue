5cdf5875cfb27e65d17f7d7dc460756604874b9d
imjohn <imjohn_ls@163.com>
2019-09-10 11:32:59 +0800

    vistor显示修改

--- src/view/chart/components/vistorInfo.vue
+++ src/view/chart/components/vistorInfo.vue
@@ -1,48 +1,53 @@
 <template>
   <div style="padding: 20px">
-    <!--固定ip-->
-    <div class="form-group">
-    <Row :gutter="30">
-      <Col span="12">
-        <div class="form-item">
-         访客状态:
-          <!--<i-switch  v-model="vistorList.dhcp"/>-->
-          <span class="ivu-switch ivu-switch-default" v-if="vistorList.visitor == 'off'"></span>
-          <span class="ivu-switch ivu-switch-checked ivu-switch-default" v-else-if="vistorList.visitor == 'on'"></span>
-        </div>
-      </Col>
-      <Col span="12">
-        <div class="form-item">
-
-        </div>
-      </Col>
-    </Row>
-    <Row :gutter="30">
-      <Col span="12">
-        <div class="form-item">
-          DNS服务地址: <span>{{vistorList.visitDuration != null ? vistorList.visitDuration : 'unknow'}}</span>
-        </div>
-      </Col>
-      <Col span="12">
-
-        <div class="form-item">
-          DNS服务地址: <span>{{vistorList.visitCount != null ? vistorList.visitCount:'unknow'}}</span>
-        </div>
-      </Col>
-    </Row>
-
+    <!--访客基础信息-->
     <Row class="list-head" type="flex" justify="space-between" align="top">
-      <Col span="6"><h3>租约信息列表:</h3></Col>
+      <Col span="6"><h3>访客基础信息:</h3></Col>
     </Row>
-    <Row class="table-container">
-      <Table :columns="table" :data="tableList" :loading="loading" height="300" stripe
-             size="small">
-        <!--   <template slot-scope="{ row }" slot="dhcpIp">
-             <span style="font-size: 12px;color: #666"><span style="color: #00e9bc;">{{ row.dhcpIp }}</span></span>
-           </template>-->
-      </Table>
-    </Row>
-  </div>
+    <div class="form-group" style="margin: 20px 0">
+      <Row :gutter="30">
+        <Col span="12">
+          <div class="form-item">
+            访客状态:
+            <!--<i-switch  v-model="vistorList.dhcp"/>-->
+            <span class="ivu-switch ivu-switch-default" v-if="vistorList.visitor === 'off'"></span>
+            <span class="ivu-switch ivu-switch-checked ivu-switch-default"
+                  v-else-if="vistorList.visitor === 'on'"></span>
+          </div>
+        </Col>
+        <Col span="12">
+          <div class="form-item">
+
+          </div>
+        </Col>
+      </Row>
+      <Row :gutter="30">
+        <Col span="12">
+          <div class="form-item">
+            DNS服务地址: <span>{{vistorList.visitDuration != null ? vistorList.visitDuration : 'unknow'}}</span>
+          </div>
+        </Col>
+        <Col span="12">
+          <div class="form-item">
+            DNS服务地址: <span>{{vistorList.visitCount != null ? vistorList.visitCount:'unknow'}}</span>
+          </div>
+        </Col>
+      </Row>
+    </div>
+    <!--列表-->
+    <div>
+      <Row class="list-head" type="flex" justify="space-between" align="top">
+        <Col span="6"><h3>访客信息列表:</h3></Col>
+      </Row>
+      <Row class="table-container">
+        <Table :columns="table" :data="tableList" :loading="loading" height="300" stripe
+               size="small">
+          <!--   <template slot-scope="{ row }" slot="dhcpIp">
+               <span style="font-size: 12px;color: #666"><span style="color: #00e9bc;">{{ row.dhcpIp }}</span></span>
+             </template>-->
+        </Table>
+      </Row>
+    </div>
   </div>
 
 </template>
@@ -59,10 +64,26 @@ export default {
         {
           type: 'index',
           width: 60
+        },
+        {
+          title: '访客IP',
+          key: 'visitorIp'
+        },
+        {
+          title: '访客MAC',
+          key: 'visitorMac'
+        },
+        {
+          title: '到期时间',
+          key: 'useTime'
+        },
+        {
+          title: '主机名',
+          key: 'hostName'
         }
       ],
       tableList: [],
-	      vistorList: []
+      vistorList: []
     }
   },
   props: {
@@ -76,18 +97,19 @@ export default {
     async getVistorInfo () {
       this.loading = true
       let res = await getVistorInfo({ nbCode: this.nbCode })
+      console.log(res)
       this.loading = false
       if (res.data.code === 'success') {
         this.tableList = res.data.result || []
       }
     },
-	    // 获取系统状态
-	    async getSystemStatus () {
-		    let res = await getSystemStatus({ nbCode: this.nbCode })
-		    if (res.data.code === 'success') {
-			    this.vistorList = res.data.result || {}
-		    }
-	    }
+    // 获取系统状态
+    async getSystemStatus () {
+      let res = await getSystemStatus({ nbCode: this.nbCode })
+      if (res.data.code === 'success') {
+        this.vistorList = res.data.result || {}
+      }
+    }
   },
   mounted () {
     this.getVistorInfo(),
@@ -97,12 +119,14 @@ export default {
 </script>
 <style lang="less" scoped>
   @import "../../config/config.less";
-  .view-content .nav-content2 .form-group .form-item{
+
+  .view-content .nav-content2 .form-group .form-item {
     font-size: 16px;
     border: none;
     padding: 10px;
   }
-  .ivu-row-flex-top{
+
+  .ivu-row-flex-top {
     margin-top: 20px;
   }
 </style>
